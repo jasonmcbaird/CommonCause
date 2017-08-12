fc9f51a5f0ea33882c122a38a81627eeae47ac49
Jasonmcbaird <jasonmcbaird@gmail.com>
2017-08-12 10:38:11 -0500

    Health and health bars now sync in multiplayer.

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
-
-	public static int maxHealth = 5;
+	[SyncVar(hook = "OnChangeHealth")]
 	public int health = maxHealth;
+	public static int maxHealth = 5;
 	public RectTransform healthBar;
 
 	public void TakeDamage(int damage)
 	{
+		if(!isServer)
+		{ return; }
 		health -= damage;
 		if(health <= 0)
 		{
 			Destroy(gameObject);
 		}
 		print(health);
+
+	}
+
+	void OnChangeHealth(int health)
+	{
 		healthBar.sizeDelta = new Vector2((float)health / (float)maxHealth * 100f, healthBar.sizeDelta.y);
 	}
 
